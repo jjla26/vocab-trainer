2ac7213974036504c066a1e651d29c6ce933abcf
Julio Lopez <julioj.lopeza@gmail.com>
2023-08-17 15:59:55 +0200

    create practice component

---
+++ src/components/Practice.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { observer } from "mobx-react"
+import { Button, Grid, TextField } from '@mui/material'
+import Card from '../components/Card'
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const Practice = () => {
+
+  return (
+    <div>Practice</div>
+  )
+}
+
+export default observer(Practice)
