8c4a41b9478e7acdc0e6d911d717cba366b0eabf
Julio Lopez <julioj.lopeza@gmail.com>
2023-08-17 15:58:08 +0200

    add vocabularies constant

---
+++ src/constants/Vocabularies.js
@@ -0,0 +1,35 @@
+const vocabularies = [
+  {
+      "id": 182,
+      "word": "car",
+      "translation": "Auto",
+      "image": "https://d1u2r2pnzqmal.cloudfront.net/vocabularies/images/182/original/Klasse6_Satzzeichen_setzen_Das_Komma_bei_Satzgef%C3%BCgen_ID_3254_Zeichenfl%C3%A4che_120210615-13353-kd1w66.svg?1623777509",
+  },
+
+  {
+      "id": 1290,
+      "word": "try",
+      "translation": "versuchen",
+      "image": "https://d1u2r2pnzqmal.cloudfront.net/vocabularies/images/1290/converted_image/24458_Mittagessen20210716-18834-1yuw7pd.png?1626445411",
+  },
+  {
+      "id": 3,
+      "word": "house",
+      "translation": "Haus",
+      "image": "https://d1u2r2pnzqmal.cloudfront.net/vocabularies/images/3/original/Batch_1_Zimmer_Zeichenfl%C3%A4che_1.svg?1627649095",
+  },
+  {
+      "id": 1,
+      "word": "you know",
+      "translation": "weißt du",
+      "image": "https://d1u2r2pnzqmal.cloudfront.net/vocabularies/images/1/original/s-genitve_listening_ID4003_Zeichenfl%C3%A4che_120210609-22535-v8qnmv.svg?1623235111",
+  },
+  {
+      "id": 445,
+      "word": "hello",
+      "translation": "Hallo",
+      "image": "https://d1u2r2pnzqmal.cloudfront.net/vocabularies/images/445/original/Blobby_framed.svg?1633424135",
+  }
+]
+
+export default vocabularies;
